fa5f369bea2d66e263c3589517b3741af05ab675
Juan-Silvestri-619 <juansilvestri.empleos@gmail.com>
2026-07-20 18:35:04 -0300

    Agregar estructura base del dominio

---
+++ domain/Autor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Autor
+    {
+        public int Id { get; private set; }
+        public string Nombre { get; private set; }
+        public string Pais { get; private set; }
+        public bool Activo { get; private set; }
+
+        //Traer información de la base de datos
+        public Autor(int id, string nombre, string pais, bool activo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Pais = pais;
+            Activo = activo;
+        }
+        //Crear un nuevo autor
+        public Autor(string nombre, string pais)
+        {
+            Nombre = ValidarTexto(nombre);
+            Pais = ValidarTexto(pais);
+            Activo = true;
+        }
+        private string ValidarTexto(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new Exception("Error: no puede estar vacío el contenido");
+            return value;
+        }
+        public void CambiarEstado(bool estado)
+        {
+            Activo = estado;
+        }
+    }
+}
---
+++ domain/Libro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public class Libro
+    {
+        public int Id { get; private set; }
+        public string Titulo { get; private set; }
+        public DateTime AnioPublicacion { get; private set; }
+        public Autor Autor { get; private set; }
+        public bool Disponible { get; private set; }
+
+        //Traer información de base de datos
+        public Libro(int id, string titulo, DateTime anio, Autor autor,  bool disponible)
+        {
+            Id = id;
+            Titulo = titulo;
+            AnioPublicacion = anio;
+            Autor = autor;
+            Disponible = disponible;
+        }
+        //Crear un nuevo libro
+        public Libro(string titulo, DateTime anio, Autor autor)
+        {
+            Titulo = ValidarTitulo(titulo);
+            AnioPublicacion = ValidarAnio(anio);
+            Autor = ValidarAutor(autor);
+            Disponible = true;
+        }
+
+        private string ValidarTitulo(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new Exception("Error: el título no puede estar vacío");
+            
+            return value;
+        }
+        private DateTime ValidarAnio(DateTime anio)
+        {
+            if (anio == default)
+                throw new Exception("Error: debe ingresar una fecha de publicación");
+
+            if (anio > DateTime.Today)
+                throw new Exception("Error: la fecha de publicación no puede ser futura");
+
+            return anio;
+        }
+        private Autor ValidarAutor(Autor autor)
+        {
+            if (autor == null)
+                throw new Exception("Error: autor vacío");
+            return autor;
+        }
+        public void CambiarDisponibilidad(bool estado)
+        {
+            Disponible = estado;
+        }
+    }
+}
