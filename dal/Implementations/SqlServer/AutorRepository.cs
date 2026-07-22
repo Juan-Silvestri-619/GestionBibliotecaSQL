baa6ba8f9c97c9af1585fef8e3b77ba689a2e508
Juan-Silvestri-619 <juansilvestri.empleos@gmail.com>
2026-07-22 20:25:48 -0300

    Se agrego la conexión a la base de datos

---
+++ dal/Implementations/SqlServer/AutorRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain;
+
+namespace dal.Implementations.SqlServer
+{
+    public class AutorRepository
+    {
+        private string _connectionString = ConfigurationManager.ConnectionStrings["connSqlServer"].ConnectionString;
+
+        public List<Autor> GetAllAuthor()
+        {
+            List<Autor> autores = new List<Autor>();
+
+            using(SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM autor";
+
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Autor autor = new Autor(
+                                Convert.ToInt32(reader["id"]),
+                                reader["nombre"].ToString(),
+                                reader["pais"].ToString(),
+                                Convert.ToBoolean(reader["activo"])
+                                );
+
+                            autores.Add(autor);
+                            
+                        }
+                    }
+                }
+                return autores;
+            }
+        }
+    }
+}
